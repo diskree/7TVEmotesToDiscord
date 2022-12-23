02d37070e0a6e91a237dc3967ddb0dd065c767e2
diskree <diskriminant.was.taken@gmail.com>
2022-12-24 02:57:30 +0400

    Download webp into cache

--- app/src/main/java/com/diskree/emotes2discord/MainActivity.kt
+++ app/src/main/java/com/diskree/emotes2discord/MainActivity.kt
@@ -4,10 +4,18 @@ import android.annotation.SuppressLint
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
+import android.os.Environment
 import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.diskree.emotes2discord.databinding.ActivityMainBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +25,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         binding.urlInputBar.addTextChangedListener {
-            if (is7TVEmoteLinkInInput(binding.urlInputBar.text.toString())) {
+            if (is7TVEmoteLink(binding.urlInputBar.text.toString())) {
                 binding.processButton.setImageResource(R.drawable.ic_done)
             } else {
                 binding.processButton.setImageResource(R.drawable.ic_copy)
@@ -27,10 +34,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.processButton.setOnClickListener {
-            if (is7TVEmoteLinkInInput(binding.urlInputBar.text.toString())) {
+            if (is7TVEmoteLink(binding.urlInputBar.text.toString())) {
                 startConversion()
             } else {
-                val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clipboardManager =
+                    getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
                 val clip = clipboardManager.primaryClip
                 if (clip != null && clip.itemCount > 0) {
                     val clipData = clip.getItemAt(0)
@@ -44,10 +52,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startConversion() {
+        val id = binding.urlInputBar.text.toString().split("emotes/")[1]
+        val baseUrl = "https://cdn.7tv.app/emote/$id/4x.webp"
+        val client = OkHttpClient()
+        val request: Request = Request.Builder().url(baseUrl).build()
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: java.io.IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val fos = FileOutputStream(File(Environment.getDownloadCacheDirectory(), "emote.webp"))
+                fos.write(response.body!!.bytes())
+                fos.close()
+            }
+        })
 
     }
 
-    private fun is7TVEmoteLinkInInput(text: String): Boolean {
+    private fun is7TVEmoteLink(text: String): Boolean {
         return text.startsWith("http://7tv.app/emotes/") || text.startsWith("https://7tv.app/emotes/")
     }
 }
